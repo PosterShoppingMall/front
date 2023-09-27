87e8844a8062dc724fd00a10766ab5e3d75c48e8
kim-hyun-mook <1mwdkim1@gmail.com>
2023-09-27 09:24:11 +0900

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Header from "./component/common/Header.jsx";
+import { Reset } from "styled-reset";
+import styled from "styled-components";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      {/* reset css  */}
+      <Reset />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
