96cf0c17a3150a9f7a0a09c9ee634054c330669e
Jun <yuncheol.kim42@gmail.com>
2023-10-07 10:55:37 +0900

    임시커밋

--- src/store/cartApi.js
+++ src/store/cartApi.js
@@ -1,5 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const cartApi = createApi({
+  reducerPath: "cart",
   baseQuery: fetchBaseQuery({ baseUrl: "http" }),
+  endpo,
 });
