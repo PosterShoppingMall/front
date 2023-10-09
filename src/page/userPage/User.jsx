52dda986dd80b160bf6f1eba6be23c6256038fe4
Jun <yuncheol.kim42@gmail.com>
2023-10-10 04:24:46 +0900

    feat:user 작업중

--- src/page/userPage/User.jsx
+++ src/page/userPage/User.jsx
@@ -1,7 +1,31 @@
+import useAxios from "./useAxios";
+import UserEdit from "./UserEdit";
 const User = () => {
-  return <div>User</div>;
+  const url = "http://localhost:3001/user/3";
+  const method = "GET";
+  const payload = null;
+  const { data, error, loaded } = useAxios(url, method, payload);
+  console.log(data);
+  return (
+    <div>
+      User
+      <UserEdit />
+    </div>
+  );
 };
 
 export default User;
 
 // 정보 조회  axios get
+
+// {
+//   "userId": 3,
+//   "name": "김찬미",
+//   "email": "test3@naver.com",
+//   "password": "12345678@",
+//   "phoneNumber": "01088883333",
+//   "postcode": "15234",
+//   "roadAddress": "강원도원주시단구동",
+//   "detailAddress": "412",
+//   "userimg":" 이미지url"
+// }
