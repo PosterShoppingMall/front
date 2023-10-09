7313362b1b9c3c51402ad4094c8bb6ec38a78835
suj2n <suj2n.k@gmail.com>
2023-10-09 18:16:13 +0900

    로그인 페이지만 있습니다

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "aos/dist/aos.css";
+import Login from "./page/login/Login.jsx";
 
 // aos 초기화 코드 추가
 import AOS from "aos";
@@ -10,5 +11,10 @@ AOS.init();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login />
   </React.StrictMode>
 );
