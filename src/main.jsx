d792732f62f3fcacde3dd40c6db92fc66df26ef5
kim-hyun-mook <1mwdkim1@gmail.com>
2023-10-09 21:50:27 +0900

    git merge

--- src/main.jsx
+++ src/main.jsx
@@ -13,8 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode></React.StrictMode>
 );
