b1367408d84963f47663e813c6749498892d92b8
suj2n <suj2n.k@gmail.com>
2023-10-12 08:59:37 +0900

    회원탈퇴버튼구현/ 로그인화면에서 회원가입페이지 연결및 로그아웃

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,19 @@ import router from "./router/router.jsx";
 import "slick-carousel/slick/slick.css"; //slick 라이브러리
 import "slick-carousel/slick/slick-theme.css"; //slick 라이브러리
 
+import { Provider } from 'react-redux'; 
+import store from './store';
+
 function App() {
   return (
+    //store 컴포넌트 묶음
+    <Provider store={store}> 
     <>
       <Reset />
       <GlobalStyle />
       <RouterProvider router={router} />
     </>
+    </Provider>
   );
 }
 
