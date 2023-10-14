0c5447d338d224848e993e7ac00766c70267493f
kim-hyun-mook <1mwdkim1@gmail.com>
2023-10-14 20:40:06 +0900

    feat:test

--- src/page/home/component/IllustrationItem.jsx
+++ src/page/home/component/IllustrationItem.jsx
@@ -56,29 +56,6 @@ const IllustrationItem = (props) => {
     ],
   };
 
-  const [items, setItems] = useState(null);
-
-  useEffect(() => {
-    // Axios를 사용하여 데이터를 가져옵니다.
-    axios
-      .get(
-        "https://shoppingmall-9c992-default-rtdb.firebaseio.com/mainitem.json"
-      )
-      .then((response) => {
-        // API에서 받아온 데이터를 객체에서 배열로 변환
-        const data = response.data;
-
-        const testData = Object.values(data);
-
-        // 변환한 데이터를 상태(state)에 저장
-        setItems(testData);
-        console.log("매핑된 데이터", testData);
-      })
-      .catch((error) => {
-        console.error("데이터를 가져오는데 실패했습니다.", error);
-      });
-  }, []);
-
   return (
     <ItemContent>
       <ContnetLine></ContnetLine>
--- src/page/home/component/MainBestItem.jsx
+++ src/page/home/component/MainBestItem.jsx
@@ -54,27 +54,6 @@ const MainBestItem = (props) => {
     ],
   };
 
-  // const [items, setItems] = useState(null);
-
-  // useEffect(() => {
-  //   // Axios를 사용하여 데이터를 가져옵니다.
-  //   axios
-  //     .get("http://52.78.184.121:8080/369/")
-  //     .then((response) => {
-  //       // API에서 받아온 데이터를 객체에서 배열로 변환
-  //       const data = response.data;
-
-  //       const testData = data.best;
-
-  //       // 변환한 데이터를 상태(state)에 저장
-  //       setItems(testData);
-  //       console.log("매핑된 데이터", testData);
-  //     })
-  //     .catch((error) => {
-  //       console.error("데이터를 가져오는데 실패했습니다.", error);
-  //     });
-  // }, []);
-
   return (
     <ItemContent>
       <ContnetLine></ContnetLine>
