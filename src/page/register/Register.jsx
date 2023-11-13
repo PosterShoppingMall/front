5ae690bee14033d00abfa46de71276c00359d78c
suj2n <suj2n.k@gmail.com>
2023-11-13 17:54:50 +0900

    회원가입,로그인 기능구현

--- src/page/register/Register.jsx
+++ src/page/register/Register.jsx
@@ -1,209 +1,206 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import axios from "axios";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { registerUser } from '../../api/api'; 
 
-const SignupContainer = styled.div`
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    password: '',
+    phoneNumber: '',
+    postCode: '',
+    roadAddress: '',
+    detailAddress: '',
+    userImg: null, // 이미지 파일
+  });
+  const [errors, setErrors] = useState({});
+  const [successMessage, setSuccessMessage] = useState('');
+  
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const validateForm = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      tempErrors.email = '유효한 이메일 주소를 입력해주세요.';
+      isValid = false;
+    }
+
+    if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/.test(formData.password)) {
+      tempErrors.password = '비밀번호는 영문자와 숫자의 조합으로 8~20자 이내여야 합니다.';
+      isValid = false;
+    }
+
+    if (!/^\d{10,11}$/.test(formData.phoneNumber)) {
+      tempErrors.phoneNumber = '유효한 전화번호를 입력해주세요.';
+      isValid = false;
+    }
+
+    if (!formData.roadAddress || !formData.detailAddress) {
+      tempErrors.address = '주소를 입력해주세요.';
+      isValid = false;
+    }
+
+    setErrors(tempErrors);
+    return isValid;
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+  
+    const data = new FormData();
+    data.append('email', formData.email);
+    data.append('name', formData.name);
+    data.append('password', formData.password);
+    data.append('phoneNumber', formData.phoneNumber);
+    data.append('postCode', formData.postCode);
+    data.append('roadAddress', formData.roadAddress);
+    data.append('detailAddress', formData.detailAddress);
+  
+    if (formData.userImg instanceof File) {
+      data.append('userImg', formData.userImg);
+    }
+  
+    try {
+      const response = await axios.post('/369/user/signup', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+      });
+  
+      setSuccessMessage('회원가입이 완료되었습니다');
+    } catch (error) {
+
+    }
+  };
+
+  return (
+    <RegisterContainer>
+      <RegisterForm onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          placeholder="이메일"
+        />
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          placeholder="이름"
+        />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          placeholder="비밀번호"
+        />
+        <input
+          type="text"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleInputChange}
+          placeholder="전화번호"
+        />
+        <input
+          type="text"
+          name="postCode"
+          value={formData.postCode}
+          onChange={handleInputChange}
+          placeholder="우편번호"
+        />
+        <input
+          type="text"
+          name="roadAddress"
+          value={formData.roadAddress}
+          onChange={handleInputChange}
+          placeholder="도로명주소"
+        />
+        {/* 상세주소 입력 필드 */}
+        <input
+          type="text"
+          name="detailAddress"
+          value={formData.detailAddress}
+          onChange={handleInputChange}
+          placeholder="상세주소"
+        />
+        <input
+          type="file"
+          name="userImg"
+          onChange={handleInputChange}
+        />
+
+        {successMessage && <p>{successMessage}</p>}
+
+        {errors.email && <p>{errors.email}</p>}
+        {errors.password && <p>{errors.password}</p>}
+        {errors.phoneNumber && <p>{errors.phoneNumber}</p>}
+        {errors.address && <p>{errors.address}</p>}
+
+         <button type="submit">회원가입</button>
+      </RegisterForm>
+    </RegisterContainer>
+  );
+};
+
+
+export default Register;
+
+const RegisterContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  background-color: #f4f4f4;
 `;
 
-const SignupForm = styled.form`
+const RegisterForm = styled.form`
   display: flex;
   flex-direction: column;
   width: 300px;
-  background-color: #f8f9fa;
   padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0px 0px 15px 1px rgba(0, 0, 0, 0.2);
-`;
-
-const Label = styled.label`
-  font-size: 16px;
-  color: #333;
-  margin-bottom: 5px;
-`;
-
-const Input = styled.input`
-  margin-bottom: 10px;
-  padding: 8px;
-  font-size: 16px;
-  border: ${({ error }) => (error ? "1px solid red" : "1px solid #ced4da")};
-  border-radius: 4px;
-  transition: border-color 0.3s;
-`;
-
-const SignupButton = styled.button`
-  font-weight: bold;
-  margin-top: 20px;
-  background-color: #000000;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  cursor: pointer;
   border-radius: 5px;
-  transition: background-color 0.3s;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: white;
+`;
 
-  &:hover {
-    background-color: #333;
+const StyledInput = styled.input`
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  &:focus {
+    outline: none;
+    border-color: #007bff;
   }
 `;
 
-const RegisterPage = () => {
-  const [formData, setFormData] = useState({
-    user: "",
-    password: "",
-    confirmPassword: "",
-    email: "",
-    name: "",
-    phoneNumber: "",
-    address: "",
-    profilePicture: null,
-  });
+const ErrorMessage = styled.p`
+  color: red;
+  font-size: 0.8rem;
+  margin-bottom: 10px;
+`;
 
-  const [error, setError] = useState({
-    user: false,
-    password: false,
-    confirmPassword: false,
-    email: false,
-    name: false,
-    phoneNumber: false,
-    address: false,
-    profilePicture: false,
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-    setError({ ...error, [e.target.name]: false });
-  };
-
-  const handleFileChange = (e) => {
-    setFormData({ ...formData, profilePicture: e.target.files[0] });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    let newErrorState = {
-      user: formData.user === "",
-      password: formData.password.length < 4,
-      confirmPassword: formData.password !== formData.confirmPassword,
-      email: !formData.email.includes("@"),
-      name: formData.name === "",
-      phoneNumber: formData.phoneNumber === "",
-      address: formData.address === "",
-      profilePicture: !formData.profilePicture
-    };
-
-    setError(newErrorState);
-
-    if (Object.values(newErrorState).some((isError) => isError)) return;
-
-    const form = new FormData();
-    Object.entries(formData).forEach(([key, value]) => {
-      form.append(key, value);
-    });
-
-    axios
-      .post("/api/signup", form)
-      .then((response) => {
-        alert("회원가입이 성공적으로 완료되었습니다");
-      })
-      .catch((error) => {
-        alert("회원가입 도중 오류가 발생했습니다");
-      });
-  };
-
- 
-  return (
-    <SignupContainer>
-      <title>회원가입</title>
-      
-      <SignupForm onSubmit={handleSubmit}>
-        <Label htmlFor="user">아이디</Label>
-        <Input 
-          type="text" 
-          id="user" 
-          name="user" 
-          placeholder="아이디를 입력하세요." 
-          error={error.name} 
-          onChange={handleChange} 
-        />
-  
-        <Label htmlFor="password">비밀번호</Label>
-        <Input 
-          type="password" 
-          id="password" 
-          name="password" 
-          placeholder="비밀번호 입력해주세요." 
-          error={error.password} 
-          onChange={handleChange} 
-        />
-  
-        <Label htmlFor="confirmPassword">비밀번호 확인</Label>
-        <Input 
-          type="password" 
-          id="confirmPassword" 
-          name="confirmPassword" 
-          placeholder="비밀번호 확인을 입력해주세요." 
-          error={error.confirmPassword} 
-          onChange={handleChange} 
-        />
-  
-        <Label htmlFor="email">이메일</Label>
-        <Input 
-          type="text" 
-          id="email" 
-          name="email" 
-          placeholder="ex) suj2n.k@email.com" 
-          error={error.email} 
-          onChange={handleChange} 
-        />
-  
-        <Label htmlFor="name">이름</Label>
-        <Input 
-          type="text" 
-          id="name" 
-          name="name" 
-          placeholder="이름을 입력해주세요." 
-          error={error.name} 
-          onChange={handleChange} 
-        />
-  
-        <Label htmlFor="phoneNumber">전화번호</Label>
-        <Input 
-          type="tel" 
-          id="phoneNumber" 
-          name="phoneNumber" 
-          error={error.phoneNumber} 
-          onChange={handleChange} 
-        />
-  
-        <Label htmlFor="address">주소</Label>
-        <Input 
-          type="text" 
-          id="address" 
-          name="address" 
-          error={error.address} 
-          onChange={handleChange} 
-        />
-  
-        <Label htmlFor="profilePicture">프로필 사진</Label>
-        <Input 
-          type="file" 
-          id="profilePicture" 
-          name="profilePicture" 
-          error={error.profilePicture} 
-          onChange={handleFileChange} 
-        />
-        
-        {error.profilePicture && <p style={{ color: "red" }}>프로필 사진을 업로드해주세요.</p>}
-  
-        <SignupButton type="submit">회원가입</SignupButton>
-      </SignupForm>
-    </SignupContainer>
-  );
-};
-
-export default RegisterPage;
+const SubmitButton = styled.button`
+  padding: 10px;
+  color: white;
+  background-color: #007bff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
